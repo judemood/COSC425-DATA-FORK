091a75c3c0035c8e576324f48f2f84722abf2e79
Jude Maggitti <judemaggitti123@gmail.com>
2024-04-27 13:18:34 -0400

    Json file of categorys and data since useing the dictionary had errors that were not resoving

--- Stats/stat.py
+++ Stats/stat.py
@@ -1,4 +1,5 @@
 import sys
+import json
 # Add the parent directory to the Python path
 sys.path.append('/mnt/linuxlab/home/jmaggitti1/425 Project/COSC425-DATA-FORK/')
 
@@ -9,8 +10,11 @@ from PythonCode.Utilities.utilities import Utilities
 if __name__ == "__main__":
     # Your code here
 
-    loader = PickleDictLoader(pickle_path="/mnt/linuxlab/home/jmaggitti1/425 Project/COSC425-DATA-FORK/PythonCode/Utilities/category_dict.pkl")
-    category_dict = loader.get_loaded_dict()
+    # loader = PickleDictLoader(pickle_path="/mnt/linuxlab/home/jmaggitti1/425 Project/COSC425-DATA-FORK/PythonCode/Utilities/category_dict.pkl")
+    # category_dict = loader.get_loaded_dict()
+    
+    with open("/mnt/linuxlab/home/jmaggitti1/425 Project/COSC425-DATA-FORK/Stats/proc_cat_data.json", "r") as cat_file:
+        category_dict = json.load(cat_file)
+    
     for key, value in category_dict.items():
         print(key)
-        print(value)
