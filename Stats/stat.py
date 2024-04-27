214b98f48eb9568d98f60dc1257cf9fa9cf59780
Jude Maggitti <judemaggitti123@gmail.com>
2024-04-27 13:58:47 -0400

    updated code with Spencer to go through the json file and grabs the name and the list of titles. It does print it to a json file

--- Stats/stat.py
+++ Stats/stat.py
@@ -5,16 +5,24 @@ sys.path.append('/mnt/linuxlab/home/jmaggitti1/425 Project/COSC425-DATA-FORK/')
 
 from GeneralUtilities.PickleDict.PickleDictLoader import PickleDictLoader
 from PythonCode.Utilities.My_Data_Classes import CategoryInfo
-from PythonCode.Utilities.utilities import Utilities
+#from PythonCode.Utilities.utilities import Utilities
 
 if __name__ == "__main__":
-    # Your code here
-
     # loader = PickleDictLoader(pickle_path="/mnt/linuxlab/home/jmaggitti1/425 Project/COSC425-DATA-FORK/PythonCode/Utilities/category_dict.pkl")
     # category_dict = loader.get_loaded_dict()
     
     with open("/mnt/linuxlab/home/jmaggitti1/425 Project/COSC425-DATA-FORK/Stats/proc_cat_data.json", "r") as cat_file:
         category_dict = json.load(cat_file)
+        # print(category_dict)
     
-    for key, value in category_dict.items():
-        print(key)
+    judes_dict = {}
+
+    for outer_key, inner_keys in category_dict.items():
+        # print(outer_key)
+        for inner_key, inner_values in inner_keys.items():
+            if inner_key == "titles":
+                judes_dict[outer_key] = inner_values
+                print(judes_dict)
+    with open("judesData.json", 'w') as file:
+        json.dump(judes_dict, file, indent=4)
+
