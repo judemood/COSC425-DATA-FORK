6a98c111f2d07bb3f6209af205b56231b40b3812
Jude Maggitti <judemaggitti123@gmail.com>
2024-04-27 12:52:39 -0400

    trying to implement the citationAvg using a dictionary but there are errors to work out

--- Stats/stat.py
+++ Stats/stat.py
@@ -1,21 +1,16 @@
 import sys
-sys.path
-sys.path.append('/mnt/linuxlab/home/jmaggitti1/425 Project/COSC425-DATA-FORK/GeneralUtilities/PickleDict/')
-sys.path.append('/mnt/linuxlab/home/jmaggitti1/425 Project/COSC425-DATA-FORK/PythonCode/Utilities/')
-# sys.path.append('/mnt/linuxlab/home/jmaggitti1/425 Project/COSC425-DATA-FORK/PythonCode/Utilities/My_Data_Classes.py')
+# Add the parent directory to the Python path
+sys.path.append('/mnt/linuxlab/home/jmaggitti1/425 Project/COSC425-DATA-FORK/')
 
 from GeneralUtilities.PickleDict.PickleDictLoader import PickleDictLoader
+from PythonCode.Utilities.My_Data_Classes import CategoryInfo
 from PythonCode.Utilities.utilities import Utilities
-from PythonCode.Utilities import My_Data_Classes
-
 
 if __name__ == "__main__":
+    # Your code here
+
     loader = PickleDictLoader(pickle_path="/mnt/linuxlab/home/jmaggitti1/425 Project/COSC425-DATA-FORK/PythonCode/Utilities/category_dict.pkl")
     category_dict = loader.get_loaded_dict()
     for key, value in category_dict.items():
         print(key)
         print(value)
-
-# how to import local python file into another python file
-# trying to import these files to use these classes but get this error
-
